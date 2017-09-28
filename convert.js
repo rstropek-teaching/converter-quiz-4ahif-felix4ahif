c67080f6f8645bf18d4e8d7ed859736ac0c6dd1c
felix4ahif <32124083+felix4ahif@users.noreply.github.com>
2017-09-28 10:28:19 +0200

    Add files via upload

--- convert.js
+++ convert.js
@@ -1,1 +1,25 @@
-console.log('Not implemented yet!');
+var error = false;
+var convert = require('convert-units');
+
+if(convert().possibilities().indexOf(process.argv[3]) <= -1 || convert().possibilities().indexOf(process.argv[5]) <= -1){
+    error = true;
+}
+
+if(process.argv.length < 6 || process.argv.length > 6){
+    error = true;
+}
+
+if(process.argv[2] <= 0 || process.argv[4] != "to"){
+    error = true;
+}
+
+if(process.argv[2] > 0){
+    if(error === false){
+        var targetUnit = convert(process.argv[2]).from(process.argv[3]).to(process.argv[5]);
+        console.log(process.argv[2] + ' ' + process.argv[3] + ' are ' + targetUnit + ' ' + process.argv[5]);
+    }else{
+        console.log("Invalid parameters");
+    }
+}else{
+    console.log("Invalid parameters");
+}
